8e3339edf201c7bd9f5ce48d8c993fd09e3fda40
scarletjasmine <scarletjasmine@proton.me>
2025-06-21 19:11:21 +0000

    Added migration for eth

--- database/migrations/2025_06_21_190429_add_ether_address_to_user.php
+++ database/migrations/2025_06_21_190429_add_ether_address_to_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user', function (Blueprint $table) {
-            //
+            $table->string("ether_address", 250)->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('user', function (Blueprint $table) {
-            //
+            $table->dropColumn("ether_address");
         });
     }
 };
