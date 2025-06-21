fb2782874ec42d2510273d1d02fa6267bd3c99d1
scarletjasmine <scarletjasmine@proton.me>
2025-06-21 18:03:45 +0000

    Added login

--- resources/js/Pages/ScarletBeast/index.tsx
+++ resources/js/Pages/ScarletBeast/index.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { Link } from '@inertiajs/react';
 
 
+
+
+
 const ScarletBeast = () => {
-  return (
-    <>
-      <h1>ScarletBeast</h1>
-      <p><Link href={route('login')} >Login</Link></p>
-  
-    </>
-    );
+  auth()->check() {
+    return (
+      <>
+        <h1>ScarletBeast</h1>
+        <p><Link href={route('logout')} >Login</Link></p>
+
+      </>
+      );
+  }
+  else {
+    return (
+      <>
+        <h1>ScarletBeast</h1>
+        <p><Link href={route('login')} >Login</Link></p>
+
+      </>
+      );
+  }
 };
+export const layout = (page: any) => <Layout children={page} title="ScarletBeast" />;
 export default ScarletBeast;
