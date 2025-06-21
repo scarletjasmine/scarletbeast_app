1603745758f05ae561b88e38b4fb106e35ae9aa6
scarletjasmine <scarletjasmine@proton.me>
2025-06-21 20:29:09 +0000

    Testing how this thing works; added field

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,8 +17,9 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-           /** 'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],*/
+            'name' => [ 'string', 'max:255'],
+            'email' => ['string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'ether_address' => ['string', 'max:255']
         ];
     }
 }
