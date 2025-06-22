09e54e9e29d18fb2c61580b48ce56bcfd7502ec8
scarletjasmine <scarletjasmine@proton.me>
2025-06-22 12:28:58 +0000

    Test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VelzonRoutesController;
+use App\Http\Controllers\CryptoController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
