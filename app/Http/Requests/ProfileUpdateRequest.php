3d2b5d6c850f99a98f6371c70b2b098e87c48384
scarletjasmine <scarletjasmine@proton.me>
2025-06-22 00:39:46 +0000

    Mm

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -20,7 +20,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => [ 'string', 'max:255'],
             'email' => ['string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'ether_address' => [new EthereumAddress],
+           // 'ether_address' => [new EthereumAddress],
         ];
     }
 }
