f088fbcbff4f3f6ecfe7dfbb6ad6b50d1bfd6836
scarletjasmine <scarletjasmine@proton.me>
2025-06-23 13:52:02 +0000

    Chat may worzXxx?

--- resources/js/Pages/ScarletBeast/index.tsx
+++ resources/js/Pages/ScarletBeast/index.tsx
@@ -3,14 +3,17 @@ import { Link } from '@inertiajs/react';
 
 
 
+interface ChatProps {
+  openaiapikey: string;
+}
 
-
-const ScarletBeast = () => {
+const ScarletBeast = (props: ChatProps) => {
     return (
       <>
         <h1>ScarletBeast</h1>
         <p><Link href={route('login')} >Login lollipop</Link></p>
         <p><Link href={route('logout')} >Logout</Link></p>
+        <Chat openaiapikey={openaiapikey} />
       </>
     );
 };
